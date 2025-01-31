05fd6040102b28ecb1ce7670ee3e48e54b18af16
Ubuntu <ubuntu@ip-172-31-82-11.ec2.internal>
2025-01-31 09:39:32 +0000

    Fresh Changes

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/api', methods=['GET'])
 def home():
-    return jsonify({"message": "Hello, Adeel! This is a simple Flask API."})
+    return jsonify({"message": "Hello, Flask ! This is a simple Flask API."})
 
 if __name__ == '__main__':
     app.run()
