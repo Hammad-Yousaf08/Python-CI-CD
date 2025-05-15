bf3d70e4b88d0f371f21c15ad207a4e7c7bcd778
Hammad@08 <hammadyousaf842@gmail.com>
2025-05-15 11:25:09 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/api', methods=['GET'])
 def home():
-    return jsonify({"message": "Hello,Flask Adeel  That's Great ! This is a simple Flask API."})
+    return jsonify({"message": "Hello,Flask Hafiz Adeel  That's Great ! This is a simple Flask API."})
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=3000)
